603c9ff5d22bfd0cea77c633d95ef544559efa00
Alex <traincrazyb@aol.com>
2014-10-13 19:40:08 -0400

    Updating script

--- script.sh
+++ script.sh
@@ -1,6 +1,10 @@
 rm -r -f ~/web/Files
+rm -r -f ~/Robocraft
+git clone https://github.com/alexbegt/Robocraft.git
+cd ~/Robocraft
 chmod +x gradlew
 ./gradlew setupCIWorkspace
 ./gradlew build
+rm -r -f ~/Web/Files
 mkdir ~/Web/Files
 cp ~/Robocraft/build/libs/* ~/Web/Files
